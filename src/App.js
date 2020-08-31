94a8ed07e38a179a58093b29ea26349c9b306bc6
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2020-09-01 00:18:09 +0300

    project files from create-react-app & homepage & homepage.styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Homepage from "./homepage";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
