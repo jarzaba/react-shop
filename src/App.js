c83c7a5d2ebd644fc327b40f85dc83160cb92deb
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2020-09-09 15:46:40 +0300

    firestore db connection and form, button tweaks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React from "react";
-import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import Homepage from "./pages/homepage/homepage";
-import Shoppage from "./pages/shop/shop.jsx";
+import Homepage from './pages/homepage/homepage';
+import Shoppage from './pages/shop/shop.jsx';
+import Header from './components/header/header.jsx';
+import SignIn_UpPage from './pages/sign-in-sign-up/sign-in-sign-up.jsx';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+//import { render } from "node-sass";
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={Shoppage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={Shoppage} />
+          <Route path='/signin' component={SignIn_UpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
